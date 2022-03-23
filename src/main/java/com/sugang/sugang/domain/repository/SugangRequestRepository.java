45ef4d926ee0741759ebdab1394bb4a1543265d1
krung2 <jungbin4337@dgsw.hs.kr>
2022-03-23 16:18:29 +0900

    Feat: sugangRequestRepository

---
+++ src/main/java/com/sugang/sugang/domain/repository/SugangRequestRepository.java
@@ -0,0 +1,7 @@
+package com.sugang.sugang.domain.repository;
+
+import com.sugang.sugang.domain.entity.SugangRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SugangRequestRepository extends JpaRepository<SugangRequest, Long> {
+}
