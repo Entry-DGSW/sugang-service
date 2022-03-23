93b3aba9c5e203cebd6dc176e5935c65eb9bf039
krung2 <jungbin4337@dgsw.hs.kr>
2022-03-23 16:15:54 +0900

    Feat: sugang Entity

---
+++ src/main/java/com/sugang/sugang/domain/entity/Sugang.java
@@ -0,0 +1,38 @@
+package com.sugang.sugang.domain.entity;
+
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "tbl_sugang")
+@DynamicInsert
+@DynamicUpdate
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Sugang extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String chargeTeacher;
+
+    @Column
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Column(columnDefinition = "DATETIME")
+    private Date startDate;
+
+    @Column(columnDefinition = "DATETIME")
+    private Date endDate;
+}
