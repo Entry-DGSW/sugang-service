e8e5bfed1ffdaea8dd21deb5132155149285461d
krung2 <jungbin4337@dgsw.hs.kr>
2022-03-23 11:34:27 +0900

    Initialize

---
+++ src/test/java/com/sugang/sugang/SugangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sugang.sugang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
