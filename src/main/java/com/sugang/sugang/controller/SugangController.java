f80eba7fe8b14e78ffe3df793c089fb0b6be36f9
krung2 <jungbin4337@dgsw.hs.kr>
2022-03-25 12:08:06 +0900

    Feat: delete sugang controller

--- src/main/java/com/sugang/sugang/controller/SugangController.java
+++ src/main/java/com/sugang/sugang/controller/SugangController.java
@@ -2,6 +2,7 @@ package com.sugang.sugang.controller;
 
 import com.sugang.sugang.controller.dto.AddSugangRequest;
 import com.sugang.sugang.service.AddSugangService;
+import com.sugang.sugang.service.DeleteSugangService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 public class SugangController {
 
     private final AddSugangService addSugangService;
+    private final DeleteSugangService deleteSugangService;
 
     @PostMapping
     public ResponseEntity<String> addSugang(
@@ -18,5 +20,13 @@ public class SugangController {
     ) {
         addSugangService.execute(addSugangRequest);
         return ResponseEntity.ok("게시 성공");
+    }
+
+    @DeleteMapping("/{sugangId}")
+    public ResponseEntity<String> deleteSugang(
+            @PathVariable("sugangId") Long sugangId
+    ) {
+        deleteSugangService.execute(sugangId);
+        return ResponseEntity.ok("강의 삭제 성공");
     }
 }
