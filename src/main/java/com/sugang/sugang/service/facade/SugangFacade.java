59eed68ff2c8422e0c61b1eb3222a6d02d56240a
krung2 <jungbin4337@dgsw.hs.kr>
2022-04-04 17:27:22 +0900

    Fix: services tie to facade

--- src/main/java/com/sugang/sugang/service/facade/SugangFacade.java
+++ src/main/java/com/sugang/sugang/service/facade/SugangFacade.java
@@ -1,2 +1,13 @@
-package com.sugang.sugang.service.facade;public class SugangFacade {
+package com.sugang.sugang.service.facade;
+
+import com.sugang.sugang.controller.dto.AddSugangDto;
+import com.sugang.sugang.controller.dto.ModifySugangDto;
+
+public interface SugangFacade {
+
+    void addSugang(AddSugangDto.Request request);
+
+    void deleteSugang(Long sugangId);
+
+    void modifySugang(Long sugangId, ModifySugangDto.Request request);
 }
--- src/main/java/com/sugang/sugang/service/facade/SugangFacadeImpl.java
+++ src/main/java/com/sugang/sugang/service/facade/SugangFacadeImpl.java
@@ -1,2 +1,32 @@
-package com.sugang.sugang.service.facade;public class SugangFacadeImpl {
+package com.sugang.sugang.service.facade;
+
+import com.sugang.sugang.controller.dto.AddSugangDto;
+import com.sugang.sugang.controller.dto.ModifySugangDto;
+import com.sugang.sugang.service.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SugangFacadeImpl implements SugangFacade {
+
+    private final AddSugangService addSugangService;
+    private final DeleteSugangService deleteSugangService;
+    private final FindSugangByIdService findSugangByIdService;
+    private final ModifySugangService modifySugangService;
+
+    @Override
+    public void addSugang(AddSugangDto.Request request) {
+        addSugangService.execute(request);
+    }
+
+    @Override
+    public void deleteSugang(Long sugangId) {
+        deleteSugangService.execute(sugangId);
+    }
+
+    @Override
+    public void modifySugang(Long sugangId, ModifySugangDto.Request request) {
+        modifySugangService.execute(findSugangByIdService.execute(sugangId), request);
+    }
 }
