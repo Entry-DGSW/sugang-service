6d9ec4462e1929f1da799aa5c6f337da679a2a99
krung2 <jungbin4337@dgsw.hs.kr>
2022-04-04 17:26:55 +0900

    Fix: add sugang service DTO parameter

--- src/main/java/com/sugang/sugang/service/AddSugangService.java
+++ src/main/java/com/sugang/sugang/service/AddSugangService.java
@@ -1,6 +1,6 @@
 package com.sugang.sugang.service;
 
-import com.sugang.sugang.controller.dto.AddSugangRequest;
+import com.sugang.sugang.controller.dto.AddSugangDto;
 import com.sugang.sugang.domain.repository.SugangRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,7 +11,7 @@ public class AddSugangService {
 
     private final SugangRepository sugangRepository;
 
-    public void execute(AddSugangRequest addSugangRequest) {
+    public void execute(AddSugangDto.Request addSugangRequest) {
         sugangRepository.save(addSugangRequest.toEntity());
     }
 }
