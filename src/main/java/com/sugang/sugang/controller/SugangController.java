5cb8f7790bead0a23f6f74286b15788292832c7b
krung2 <jungbin4337@dgsw.hs.kr>
2022-04-04 17:27:49 +0900

    Fix: services to facade in controller

--- src/main/java/com/sugang/sugang/controller/SugangController.java
+++ src/main/java/com/sugang/sugang/controller/SugangController.java
@@ -1,8 +1,8 @@
 package com.sugang.sugang.controller;
 
-import com.sugang.sugang.controller.dto.AddSugangRequest;
-import com.sugang.sugang.service.AddSugangService;
-import com.sugang.sugang.service.DeleteSugangService;
+import com.sugang.sugang.controller.dto.AddSugangDto;
+import com.sugang.sugang.controller.dto.ModifySugangDto;
+import com.sugang.sugang.service.facade.SugangFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,13 +12,13 @@ import org.springframework.web.bind.annotation.*;
 public class SugangController {
 
     private final AddSugangService addSugangService;
-    private final DeleteSugangService deleteSugangService;
+    private final SugangFacade sugangFacade;
 
     @PostMapping
     public ResponseEntity<String> addSugang(
-            @RequestBody AddSugangRequest addSugangRequest
+            @RequestBody AddSugangDto.Request request
     ) {
-        addSugangService.execute(addSugangRequest);
+        sugangFacade.addSugang(request);
         return ResponseEntity.ok("게시 성공");
     }
 
@@ -26,7 +26,16 @@ public class SugangController {
     public ResponseEntity<String> deleteSugang(
             @PathVariable("sugangId") Long sugangId
     ) {
-        deleteSugangService.execute(sugangId);
+        sugangFacade.deleteSugang(sugangId);
         return ResponseEntity.ok("강의 삭제 성공");
+    }
+
+    @PutMapping("/{sugangId}")
+    public ResponseEntity<String> modifySugang(
+            @PathVariable("sugangId") Long sugangId,
+            @RequestBody ModifySugangDto.Request request
+    ) {
+        sugangFacade.modifySugang(sugangId, request);
+        return ResponseEntity.ok("강의 수정 성공");
     }
 }
